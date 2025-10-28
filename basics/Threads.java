166475955736096c081afef492b74a9545668647
Aishwarya S <aishwarya7sk@gmail.com>
2025-10-28 13:55:01 +0530

    Add initial documentation for Threads.java

---
+++ basics/Threads.java
@@ -0,0 +1,13 @@
+// Java thread is the smallest unit of execution within a program. 
+// It is a lightweight subprocess that runs independently but shares the same memory space as the process, allowing multiple tasks to execute concurrently.
+
+// Thread Creation Methods
+// 1. By Extending the Thread Class
+// 2. By Implementing the Runnable Interface
+// Note -> Extend Thread when you don’t need to extend any other class. Implement Runnable when your class already extends another class.
+// Thread Life Cycle
+// New: Thread object is created but not started.
+// Runnable: Thread is ready to run and waiting for CPU allocation.
+// Running: Thread is executing its run() method.
+// Waiting/Blocked: Thread waits for a resource or another thread.
+// Terminated: Thread completes execution or is stopped.
