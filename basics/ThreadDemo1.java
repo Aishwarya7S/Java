18f2543925025450f5e7e2cf476cc6338151e438
Aishwarya S <aishwarya7sk@gmail.com>
2025-10-28 14:12:17 +0530

    Rename ThreadExample1 to ThreadDemo1

--- basics/ThreadDemo1.java
+++ basics/ThreadDemo1.java
@@ -5,7 +5,7 @@ class MyThread extends Thread {
     }
 }
 
-public class ThreadExample1 {
+public class ThreadDemo1 {
     public static void main(String[] args) {
         MyThread t1 = new MyThread();
         t1.start();
