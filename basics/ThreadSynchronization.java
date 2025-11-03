527030a0551a950a4e28b753bd8341463ca6fe62
Aishwarya S <aishwarya7sk@gmail.com>
2025-11-03 11:03:11 +0530

    Update ThreadSynchronization.java

--- basics/ThreadSynchronization.java
+++ basics/ThreadSynchronization.java
@@ -2,7 +2,7 @@
 // Synchronization - ensures only one thread accesses it at a time.\
 
 //Types - 2
-// 1. Mutual Exclusion => synchronized methods, synchronized blocks , synchronized static methods
+// 1. Mutual Exclusion (Synchronization) => synchronized methods, synchronized blocks , synchronized static methods
 // 2. Cooperation (Inter-thread comm) => wait(),  notify(), notifyAll()
 
 // 1
@@ -30,6 +30,13 @@ public class ThreadSynchronization {
 }
 
 // 2 
+// allows threads to coordinate their actions rather than just running independently.
 // wait() → pauses the thread until another thread calls notify().
 // notify() → wakes up one waiting thread.
 // notifyAll() → wakes up all waiting threads.
+
+// Eg: Producer–Consumer problem, where:
+// The Producer thread generates data.
+// The Consumer thread uses that data.
+// wait() and notify() handle the coordination between them safely.
+// => PC Program Flow  === The Producer creates an item and calls notify() to wake up the Consumer.The Consumer waits if no item is available and calls notify() after consuming.Both threads cooperate instead of competing — ensuring data consistency and no race conditions.
